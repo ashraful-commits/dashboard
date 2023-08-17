4745b1867ad142cee8d88f4e92cfe57ed9af9f25
ashraful-commits <ashrfulalam10000@gmail.com>
2023-08-17 18:16:31 +0600

    monog db

--- api/config/mongoDBConncetion.js
+++ api/config/mongoDBConncetion.js
@@ -6,9 +6,9 @@ import colors from "colors";
 //====================create mongodb conncetion 
 
 
-export const MongoDBCnncetion =()=>{
+export const MongoDBCnncetion =async()=>{
     try {
-        const conncetion = mongoose.connect(process.env.MOGO_SERVER)
+        const conncetion = await mongoose.connect(process.env.MOGO_SERVER)
         console.log(`MongoDB connection successfully !`.bgGreen.cyan)
     } catch (error) {
         console.log(error.message)
